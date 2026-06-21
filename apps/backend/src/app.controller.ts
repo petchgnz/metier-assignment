80f1d7ce3705d832d4e976445be0956a9e6aca75
petchgnz (desktop) <phetchgnz@gmail.com>
2026-06-21 13:23:32 +0700

    fix: change PartiaType import source
    - changed from 'mapped-type' to '@nestjs/swagger'. this will make the dto showing in swagger correctly

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('health')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  health() {
+    return {
+      status: 'ok',
+    };
   }
 }
