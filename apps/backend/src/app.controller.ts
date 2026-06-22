114a4cbc095be5c5a1618537bb792afea3059f58
petchgnz (desktop) <phetchgnz@gmail.com>
2026-06-22 12:56:31 +0700

    deploy: prepare backend

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,14 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller('health')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
+  @Get('health')
   health() {
-    return {
-      status: 'ok',
-    };
+    return { status: 'ok' };
   }
 }
